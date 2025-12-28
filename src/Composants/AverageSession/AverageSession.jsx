e52601071d72398830ec283f4345207216ef1a9d
Emmie Gemme <mimiemmie@gmail.com>
2025-12-28 12:21:15 -0500

    AverageSession en cours

--- src/Composants/AverageSession/AverageSession.jsx
+++ src/Composants/AverageSession/AverageSession.jsx
@@ -1,29 +1,54 @@
-import { AreaChart, Area, XAxis, YAxis, ResponsiveContainer, Tooltip } from 'recharts';
+import { AreaChart, Area, XAxis, YAxis, ResponsiveContainer, Tooltip, Rectangle, LineChart, Line} from 'recharts';
 import "./AverageSession.scss"
-
+const CustomCursor = (props) => {
+  const { points, width, height } = props;
+  const { x, y } = points[0];
+  
+  console.log(props);
+  return (
+    <Rectangle
+      fill="#E60000"
+      zIndex={1000}
+      x={x}
+      y={y}
+      width={width}
+      height={height}
+    />
+  );
+};
 const AverageSessionAreaChart = ({data}) => {
   return (
     <div className="average-session" style={{ backgroundColor: "#FF0000", fontSize: "15px", fontWeight: "500"}}>
         <h1 className="chart-title">Durée moyenne des sessions</h1>
-        <ResponsiveContainer width="100%" height={190}>
+        <ResponsiveContainer width="100%" height={263}>
+            
             <AreaChart
                 data={data}
-                margin={{bottom: 5}}
+                margin={{bottom: 0}}
+              
             >
+            <defs>
+                <linearGradient id="colorStroke" x1="0" y1="0" x2="1" y2="0">
+                    <stop offset="0%" stopColor="#FFFFFF" stopOpacity={1}/>
+                    <stop offset="81%" stopColor="#FFFFFF" stopOpacity={0} />
+                </linearGradient>
+            </defs>
             <XAxis 
                 dataKey="day"
                 axisLine={false}
                 tickLine={false}  
-                className="average-session_x"
-                padding={{left: 14, right: 14}}
+                padding={{left: 19, right: 19}}
+                
+                
             />
             <YAxis 
-            width="auto" 
-            hide={true} 
-            margin={19}
-            domain={[0, 'dataMax + 10']} 
-            padding={{bottom : 20}}
-           
+                width="auto" 
+                hide={true} 
+                margin={0}
+                domain={[0, 'dataMax + 30']} 
+                allowDataOverflow 
+                
+              
             
             />
             <Tooltip 
@@ -31,11 +56,9 @@ const AverageSessionAreaChart = ({data}) => {
                     color: '#000000F',
                     margin: 0,
                     fontSize: 8,
-                    
                     padding: 0
-                }}
-                cursor={{ stroke: "#E60000", strokeWidth: 50, opacity: 9.75}}
-                
+                }}               
+                cursor={<CustomCursor/>}
                 labelFormatter={() => ""}
                 formatter={(value, name) =>
                     name === "time"
@@ -43,7 +66,17 @@ const AverageSessionAreaChart = ({data}) => {
                         : [""]
                     }
             />
-            <Area type="natural" dataKey="time" stroke="#FFFFFF" fill="#FF0C0C" strokeWidth={2}/>
+            <Area 
+                type="natural" 
+                activeDot={{ stroke: "rgba(225, 225, 225, 30%)", strokeWidth: 9, r: 4 }} 
+                dataKey="time" 
+                stoke="#FFFFFF"
+                fill="url(#colorStroke)" 
+                strokeWidth={2}
+                margin={0} 
+                zIndex={5000}
+                
+            />
             </AreaChart>
         </ResponsiveContainer>
     </div>
