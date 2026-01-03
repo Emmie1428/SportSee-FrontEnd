17880ff88b3bd0614aabca7dacf9bef7df94d988
Emmie Gemme <mimiemmie@gmail.com>
2026-01-03 18:39:51 -0500

    Feat AverageSession terminé imparfait

--- src/Composants/AverageSession/AverageSession.jsx
+++ src/Composants/AverageSession/AverageSession.jsx
@@ -1,45 +1,41 @@
-import { AreaChart, Area, XAxis, YAxis, ResponsiveContainer, Tooltip, Rectangle, LineChart, Line} from 'recharts';
+import { AreaChart, Area, XAxis, YAxis, ResponsiveContainer, Tooltip, Rectangle} from 'recharts';
 import "./AverageSession.scss"
+
 const CustomCursor = (props) => {
   const { points, width, height } = props;
   const { x, y } = points[0];
-  
-  console.log(props);
   return (
     <Rectangle
       fill="#E60000"
-      zIndex={1000}
       x={x}
       y={y}
       width={width}
       height={height}
     />
-  );
-};
+  )
+}
+
 const AverageSessionAreaChart = ({data}) => {
   return (
-    <div className="average-session" style={{ backgroundColor: "#FF0000", fontSize: "15px", fontWeight: "500"}}>
+    <div className="average-session" style={{ background: "linear-gradient(to top, #FF1A1A 5%, #FF0000 90%)", fontSize: "15px", fontWeight: "500"}}>
         <h1 className="chart-title">Durée moyenne des sessions</h1>
         <ResponsiveContainer width="100%" height={263}>
-            
             <AreaChart
                 data={data}
-                margin={{bottom: 0}}
-              
+                margin={{bottom: 10}}
             >
             <defs>
-                <linearGradient id="colorStroke" x1="0" y1="0" x2="1" y2="0">
-                    <stop offset="0%" stopColor="#FFFFFF" stopOpacity={1}/>
-                    <stop offset="81%" stopColor="#FFFFFF" stopOpacity={0} />
+                <linearGradient id="colorStroke" x1="1" y1="0" x2="0" y2="0">
+                    <stop offset="0%" stopColor="#FFFFFF" strokeOpacity={1} />
+                    <stop offset="81%" stopColor="#FF6666" strokeOpacity={.4} />
                 </linearGradient>
             </defs>
             <XAxis 
                 dataKey="day"
                 axisLine={false}
-                tickLine={false}  
-                padding={{left: 19, right: 19}}
-                
-                
+                tickLine={false}   
+                interval="preserveStartEnd"
+                cursor={<CustomCursor/>}       
             />
             <YAxis 
                 width="auto" 
@@ -47,40 +43,37 @@ const AverageSessionAreaChart = ({data}) => {
                 margin={0}
                 domain={[0, 'dataMax + 30']} 
                 allowDataOverflow 
-                
-              
-            
             />
             <Tooltip 
                 itemStyle={{ 
-                    color: '#000000F',
+                    color: "#000000/",
                     margin: 0,
                     fontSize: 8,
                     padding: 0
                 }}               
-                cursor={<CustomCursor/>}
+                cursor={<CustomCursor/>}   
                 labelFormatter={() => ""}
                 formatter={(value, name) =>
                     name === "time"
                         ? [`${value} min`]
                         : [""]
-                    }
+                }
             />
             <Area 
                 type="natural" 
                 activeDot={{ stroke: "rgba(225, 225, 225, 30%)", strokeWidth: 9, r: 4 }} 
                 dataKey="time" 
-                stoke="#FFFFFF"
-                fill="url(#colorStroke)" 
+                stroke="url(#colorStroke)"
+                fill="#FF1A1A"
                 strokeWidth={2}
                 margin={0} 
-                zIndex={5000}
-                
+                width="100%"
+                zIndex={50000}
             />
             </AreaChart>
         </ResponsiveContainer>
     </div>
-  );
-};
+  )
+}
 
 export default AverageSessionAreaChart;
--- src/Models/AverageSessions.js
+++ src/Models/AverageSessions.js
@@ -6,7 +6,7 @@ class AverageSessions {
     }
 
     get formattedDuration() {
-        const dayLetter = {1:"L", 2:"M", 3:"M", 4:"J", 5:"V", 6:"S", 7:"D"}
+        const dayLetter = {1:"    L", 2:"M", 3:"M", 4:"J", 5:"V", 6:"S", 7:"D    "}
         return this.sessions.map((session) => ({
             day: dayLetter[session.day],
             time: session.sessionLength
